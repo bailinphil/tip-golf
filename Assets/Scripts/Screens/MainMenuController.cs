964b59d5fcb1c65f18c90b587a0987c1d42be531
plight <plight@gmail.com>
2014-10-08 15:10:15 -0400

    made background for mainscreen, button, loads scene 'Andrews1' (which is empty)

---
+++ Assets/Scripts/Screens/MainMenuController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+	void OnGUI(){
+		if (GUI.Button(new Rect(60, 140, 300, 60), "New Game"))
+			Application.LoadLevel ( "Andrews1" );
+	}
+}
