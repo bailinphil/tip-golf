b8d11965e422af657014843c83b37eb10f10ea71
plight <plight@gmail.com>
2014-10-08 16:22:20 -0400

    created first 'hole' and set up prefab with gyro script

---
+++ Assets/Scripts/BoundsTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using Lib;
+
+public class BoundsTrigger : MonoBehaviour {
+
+	void OnTriggerExit( Collider other ){
+		print ( "out of bounds" );
+		Messenger.Broadcast( "OutOfBounds" );
+		Application.LoadLevel( Application.loadedLevel );
+	}
+}
---
+++ Assets/Scripts/PlayerTrackingLight.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTrackingLight : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	void Start(){
+		offset = transform.position;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
