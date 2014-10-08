dc04626a3ea0ff442eac97cfa960edba7dfae281
plight <plight@gmail.com>
2014-10-08 17:55:55 -0400

    bringing camera along with the ball

--- Assets/Scripts/BoundsTrigger.cs
+++ Assets/Scripts/BoundsTrigger.cs
@@ -5,7 +5,6 @@ using Lib;
 public class BoundsTrigger : MonoBehaviour {
 
 	void OnTriggerExit( Collider other ){
-		print ( "out of bounds" );
 		Messenger.Broadcast( "OutOfBounds" );
 		Application.LoadLevel( Application.loadedLevel );
 	}
---
+++ Assets/Scripts/TrackPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackPlayer : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	void Start () {
+		offset = transform.localPosition - player.transform.localPosition;
+	}
+	
+	void LateUpdate () {
+		transform.localPosition = player.transform.localPosition + offset;
+	}
+}
